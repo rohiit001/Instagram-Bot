eb655a22d33274e3382dc19cd5c504073d037c32
rohiit001 <70051236+rohiit001@users.noreply.github.com>
2023-09-29 21:55:01 +0530

    config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ class Config:
     API_ID = int(os.environ.get("API_ID", "23735332"))
     API_HASH = os.environ.get("API_HASH", "08e4c9a3dbc9834d730c2a8887b718ab")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6230278516:AAHhaGfRvYMxK4OckftED9KPgadnGIG79_U") 
-    USER = os.environ.get("INSTAGRAM_USERNAME", "ying.zon")
+    USER = os.environ.get("INSTAGRAM_USERNAME", "bfm_skills")
     OWNER = os.environ.get("OWNER_ID", "6519349716")
-    INSTA_SESSIONFILE_ID = os.environ.get("INSTA_SESSIONFILE_ID", None)
+    INSTA_SESSIONFILE_ID = os.environ.get("INSTA_SESSIONFILE_ID", "37543643306%3AqijTaaz7AEAWlq%3A21%3AAYdBSJuEuUmxZHzVR71-ESkLo6qLKdKVoLBPobJAdg")
     S = "0"
     STATUS = set(int(x) for x in (S).split())
     L=Instaloader()
